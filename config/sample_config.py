d819963cbd7150cc51391b42e1962af2c16646f9
trdrive <80867306+trdrive@users.noreply.github.com>
2021-04-28 17:55:22 +0530

    Update sample_config.py

--- config/sample_config.py
+++ config/sample_config.py
@@ -1,17 +1,17 @@
 # You can copy this file to config.py and add your info
 
 # Get API ID and HASH from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = "4810065"
+API_HASH = "01d3d24dcbbfba7189ee7c506f1a0fdb"
 
 # Get this from @Botfather
-BOT_TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+BOT_TOKEN = "1790128013:AAEMEHyagWwfUMZqT2AFjRiOxKezsfJV3y4"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
+    1413821482,
     951435494,
     1392620345
 ]
 
-CHAT_ID = -1001243271409  # ID of the group where the bot streams
+CHAT_ID = -100457440313  # ID of the group where the bot streams
