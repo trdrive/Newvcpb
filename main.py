68df82f8e28c55f2b4f197985fdc0b473ac55f95
trdrive <80867306+trdrive@users.noreply.github.com>
2021-04-28 18:32:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from config import API_ID, API_HASH, BOT_TOKEN
 bot = Client(
     "my_account",
     api_id=4810065,
-    api_hash= 01d3d24dcbbfba7189ee7c506f1a0fdb
-    bot_token=1790128013:AAESIats1yXV3fSb9XPcnjIPfSKCc466JdA
+    api_hash="01d3d24dcbbfba7189ee7c506f1a0fdb"
+    bot_token="1790128013:AAESIats1yXV3fSb9XPcnjIPfSKCc466JdA"
     plugins={"root": "handlers"}
 )
 
